0b73f2928746aa3586c334bd04882a0c1146cf89
James Openshaw <open0008@hz.nl>
2025-02-11 18:40:18 +0100

    composer npm and added images

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,57 @@
+@vite(['resources/sass/blog.scss'])
+
+<x-layout.main>
+    <!--Blog posts-->
+    <section class="post-head">
+        <div class="post-summary">
+            <div>
+                <img src="images/study-choice.jpg">
+            </div>
+            <div style="margin-right: 95px;">
+                <h2>Study Choice</h2>
+                <p>Why I chose this study and what I'll be doing in the future!</p>
+                <a href="/blog-content/study-choice.html">read more</a>
+            </div>
+        </div>
+        <div class="post-summary">
+            <div>
+                <img src="images/swot.jpg">
+            </div>
+            <div style="margin-right: 245px;">
+                <h2>Personal SWOT Analysis</h2>
+                <p>The results from my SWOT analysis.</p>
+                <a href="/blog-content/swot-analysis.html">read more</a>
+            </div>
+        </div>
+        <div class="post-summary">
+            <div>
+                <img src="images/experience.jpg">
+            </div>
+            <div style="margin-right: 122px;">
+                <h2>Programming Experience</h2>
+                <p>My programming experience before coming to the HZ.</p>
+                <a href="blog-content/experience.html">read more</a>
+            </div>
+        </div>
+        <div class="post-summary">
+            <div>
+                <img src="images/feedback.jpg">
+            </div>
+            <div style="margin-right: 55px;">
+                <h2>First Feedback</h2>
+                <p>The feedback I received after filling in the "Who Am I?" questionnaire.</p>
+                <a href="blog-content/first-feedback.html">read more</a>
+            </div>
+        </div>
+        <div class="post-summary">
+            <div>
+                <img src="images/ict-careers.jpg">
+            </div>
+            <div>
+                <h2>Careers in ICT</h2>
+                <p>A little bit about a few ICT careers that I find interesting.</p>
+                <a href="/blog-content/ict-field.html">read more</a>
+            </div>
+        </div>
+    </section>
+</x-layout.main>
