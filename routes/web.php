6b0fd262f6c7675a218d56f787033438207346ce
James Openshaw <open0008@hz.nl>
2025-02-11 19:07:09 +0100

    added routes to web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 })->name('home');
 
+Route::get('/profile', function () {
+    return view('profile');
+})->name('profile');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::get('/blog', function () {
+    return view('blog');
+})->name('blog');
+
+Route::get('/faq', function () {
+    return view('faq');
+})->name('faq');
